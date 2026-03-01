dbf9149f35c991f4b1d2c13a2f1276b378b356a8
Helena Januszki <helena.januszki@gmail.com>
2026-03-01 13:17:44 -0500

    oops another fix

--- backend/main.py
+++ backend/main.py
@@ -45,7 +45,7 @@ def update_contact(user_id):
 
     return jsonify({"message": "User updated"}), 200
 
-@app.route("/delete_contact/<int=user_id>", methods=["DELETE"])
+@app.route("/delete_contact/<int:user_id>", methods=["DELETE"])
 def delete_contact(user_id):
     contact = Contact.query.get(user_id)
 
